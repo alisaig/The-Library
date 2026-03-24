e1207aff794f99904994734dafa6c6d8fecbc494
alisaig <alisa.ign20@gmail.com>
2026-03-24 20:03:32 +0100

    Add more book info and complexity to form
    Break date category into start and end date.
    Add logic to form so start, end date and rating
    fields and values become disabled and wipe when
    certain status options are chosen

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 // Map storing every book, using randomly generated ids as keys
-const headings = ["title", "author", "rating", "status", "pages", "date"];
+const headings = ["title", "author", "pages", "status", "startDate", "endDate", "rating"];
 const books = new Map();
 
 // Book object 
-function Book(title, author, rating, status, pages, date) {
+function Book(title, author, pages, status, startDate, endDate, rating) {
     this.title = title;
     this.author = author;
-    this.rating = rating;
-    this.status = status;
     this.pages = pages;
-    this.date = date;
+    this.status = status;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.rating = rating;
     // Generate random id unique to each book
     this.id = crypto.randomUUID();
 };
 
 // Function to take book info, create a Book object from it and add it to the books map
-function addBookToLibrary(title, author, rating, status, pages, date) {
-    const book = new Book(title, author, rating, status, pages, date);
+function addBookToLibrary(title, author, pages, status, startDate, endDate, rating) {
+    const book = new Book(title, author, pages, status, startDate, endDate, rating);
     books.set(book.id, book);
 };
 
 // Books added as default to test layout
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 4, "read", 400, "14 Feb 2016");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 400, "read", "14 Feb 2016", "7 Mar 2016", 4);
 
-addBookToLibrary("Of Mice and Men", "John Steinbeck", 3.5, "read", 107, "4 Nov 2014");
+addBookToLibrary("Of Mice and Men", "John Steinbeck", 107, "read", "4 Nov 2014", "26 Nov 2014", 3.5);
 
 
 function displayBooks() {
@@ -56,36 +57,51 @@ const startDateField = document.querySelector("#start-date");
 const endDateField = document.querySelector("#end-date");
 const ratingFields = document.querySelector(".rating-group");
 
+const stars = document.querySelectorAll(".star");
+
+function starReset() {
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove("filled");
+    }
+}
+
 // Update the form to only show certain fields depending on the value of status
+
 
 function statusDependentFormUpdate() {
     const currentStatus = statusField.value;
 
     switch(currentStatus) {
         case "to-read":
-            startDateField.disabled = "true";
-            endDateField.disabled = "true";
-            ratingFields.disabled = "true";
+            startDateField.value = "";
+            endDateField.value = "";
+            starReset();
+            
+            startDateField.disabled = true;
+            endDateField.disabled = true;
+            ratingFields.disabled = true;
             break;
+
         case "reading":
-            startDateField.disabled = "true";
-            endDateField.disabled = "false";
-            ratingFields.disabled = "true";
+            startDateField.disabled = false;
+            endDateField.disabled = true;
+            ratingFields.disabled = false;
             break;
+
         case "read":
-            startDateField.disabled = "false";
-            endDateField.disabled = "false";
-            ratingFields.disabled = "false";
+            startDateField.disabled = false;
+            endDateField.disabled = false;
+            ratingFields.disabled = false;
             break;
     }
 }
+
 
 statusDependentFormUpdate()
 
 statusField.addEventListener("change", statusDependentFormUpdate);
 
 // Event listener to visually display rating in form when adding a new book
-const stars = document.querySelectorAll(".star");
 
 ratingFields.addEventListener("change", (event) => {
     const rating = parseInt(event.target.value);
